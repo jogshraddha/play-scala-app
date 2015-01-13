f6348990d141f8f211c0003580965ad67d699031
Shraddha <shraddha@ULTP-342.(none)>
2015-01-13 16:12:12 +0530

    added register end point

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,14 @@ import play.api.mvc._
 object Application extends UserController {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("'Your new application is ready."))
   }
+
+  def register =  Action(parse.json) { implicit request =>
+    Ok("Registered")
+  }
+  
+
+  def update(name : String) = TODO
 
 }
